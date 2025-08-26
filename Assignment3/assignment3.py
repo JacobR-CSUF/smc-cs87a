78d3ba2015b4acd90a659a7966729d4c2c8b0740
JacobR-CSUF <157658230+JacobR-CSUF@users.noreply.github.com>
2025-08-26 11:10:05 -0700

    Fix indentation for return statements and main function

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,37 +1,37 @@
 # CS 87A Lu -- Module 4 Assignment
 # STUDENT NAME: Jacob Rodas
 def miles_to_meters(miles):
-meters = miles * 1609.344
-return meters
+  meters = miles * 1609.344
+  return meters
 
 def kilometers_to_meters(kilometers):
-meters = kilometers * 1000
-return meters
+  meters = kilometers * 1000
+  return meters
 
 def centimeters_to_meters(centimeters):
-meters = centimeters / 100
-return meters
+  meters = centimeters / 100
+  return meters
 
 def millimeters_to_meters(millimeters):
-meters = millimeters / 1000
-return meters
+  meters = millimeters / 1000
+  return meters
 
 #prompt user for distance and unit type (mi/km/cm/mm) to convert to meters.
 def main():
-num_units = float(input("How many units are you converting: "))
-unit_type = input("Which units are you converting from (mi/km/cm/mm): ")
+  num_units = float(input("How many units are you converting: "))
+  unit_type = input("Which units are you converting from (mi/km/cm/mm): ")
 
 #determine unit type, then use appropriate function to calculate into meters.
-if unit_type == "mi":
-print(miles_to_meters(num_units), "meters")
-elif unit_type == "km":
-print(kilometers_to_meters(num_units), "meters")
-elif unit_type == "cm":
-print(centimeters_to_meters(num_units), "meters")
-elif unit_type == "mm":
-print(millimeters_to_meters(num_units), "meters")
-else:
-print("Invalid unit type.")
+  if unit_type == "mi":
+    print(miles_to_meters(num_units), "meters")
+  elif unit_type == "km":
+    print(kilometers_to_meters(num_units), "meters")
+  elif unit_type == "cm":
+    print(centimeters_to_meters(num_units), "meters")
+  elif unit_type == "mm":
+    print(millimeters_to_meters(num_units), "meters")
+  else:
+    print("Invalid unit type.")
 
 if __name__ == "__main__":
 main()
